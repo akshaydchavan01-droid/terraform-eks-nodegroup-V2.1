bcb68c736ddd62836025bda44a90748aa8d10476
Akshay Chavan <akshaydchavan01@gmail.com>
2026-04-09 16:28:20 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
     # NEW: Add Kubernetes provider for aws-auth ConfigMap
     kubernetes = {
