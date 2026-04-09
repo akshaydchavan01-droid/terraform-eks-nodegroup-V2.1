5e9c938c1c1451d7a66943726de9dd83c832b7d5
Akshay Chavan <akshaydchavan01@gmail.com>
2026-04-09 16:32:05 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -30,7 +30,7 @@ provider "aws" {
 provider "kubernetes" {
   host                   = aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_auth.cluster.token
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 # NEW: Get authentication token for Kubernetes provider
